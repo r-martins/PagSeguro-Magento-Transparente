59e7a2c2ac0aaacf0fc55a88e09dcc5bb7ef3239
Ricardo Martins <ricardo@ricardomartins.info>
2014-07-16 08:17:56 -0300

    Correcao de imagem e criacao do buildjekyll

---
+++ buildjekyll.sh
@@ -0,0 +1,4 @@
+jekyll build --source __jekyll --destination __jekyll/_site
+cp -R __jekyll/_site/* .
+echo "pra rodar é sóigitar "
+echo "jekyll serve --source __jekyll/ --destination __jekyll/_site"
