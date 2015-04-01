74f1ac14611f1949e97b069fffa2f2bf97ab9959
Eneias Ramos de Melo <eneias@gamuza.com.br>
2015-04-01 19:58:03 -0300

    Adicionado loading para obtencao de hash via ajax

---
+++ skin/frontend/base/default/js/ricardomartins_pagseguro.js
@@ -0,0 +1,11 @@
+function pagseguroDocumentHeight()
+{
+    var D = document;
+
+    return Math.max(
+        D.body.scrollHeight, D.documentElement.scrollHeight,
+        D.body.offsetHeight, D.documentElement.offsetHeight,
+        D.body.clientHeight, D.documentElement.clientHeight
+    );
+}
+
