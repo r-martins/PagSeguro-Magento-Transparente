f2a572c4d96c297fc96c74db0f4623aa73def8a6
Ricardo Martins <ricardo@ricardomartins.net.br>
2016-09-21 20:47:08 -0300

    Site novo com Themler

--- buildjekyll.sh
+++
@@ -1,4 +0,0 @@
-jekyll build --source __jekyll --destination __jekyll/_site
-cp -R __jekyll/_site/* .
-echo "pra rodar é sóigitar "
-echo "jekyll serve --source __jekyll/ --destination __jekyll/_site"
