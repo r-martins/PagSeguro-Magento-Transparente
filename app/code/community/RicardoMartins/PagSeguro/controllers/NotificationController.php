4eeb65b235201637d55bda1d72537baac104cbb6
Ricardo Martins <ricardo@ricardomartins.net.br>
2017-10-10 17:04:49 +1100

    v. 3.4.0 - Corrigido problema das faturas duplicadas quando o Pagseguro enviava 2 ou mais notificacoes iguais no mesmo segundo (Fix #215) - Acrescentado eventos para checar se as configuracoes basicas do modulo estao corretas, evitando dores de cabeca - Pagina amigavel em /pseguro/notification, permite testar uma notificacao quando nao se envia o notificationCode

--- app/code/community/RicardoMartins/PagSeguro/controllers/NotificationController.php
+++ app/code/community/RicardoMartins/PagSeguro/controllers/NotificationController.php
@@ -22,6 +22,24 @@ class RicardoMartins_PagSeguro_NotificationController extends Mage_Core_Controll
         if ($helper->isSandbox()) {
             $this->getResponse()->setHeader('access-control-allow-origin', 'https://sandbox.pagseguro.uol.com.br');
         }
+
+        if ( $this->getRequest()->getPost('notificationCode', false) == false) {
+            $this->getResponse()->setHttpResponseCode(422);
+            $this->loadLayout();
+            $this->renderLayout();
+            return;
+        }
+        $notificationCode = $this->getRequest()->getPost('notificationCode');
+
+        //Workaround for duplicated PagSeguro notifications (Issue #215)
+        $exists = Mage::app()->getCache()->load($notificationCode);
+        if($exists){
+            $this->getResponse()->setHttpResponseCode(400);
+            $this->getResponse()->setBody('Notificação já enviada a menos de 1 minuto.');
+            return;
+        }
+        Mage::app()->getCache()->save('in_progress', $notificationCode, array('pagseguro_notification'), 60);
+
         /** @var RicardoMartins_PagSeguro_Model_Abstract $model */
         Mage::helper('ricardomartins_pagseguro')
             ->writeLog(
@@ -29,12 +47,14 @@ class RicardoMartins_PagSeguro_NotificationController extends Mage_Core_Controll
                 . var_export($this->getRequest()->getParams(), true)
             );
         $model =  Mage::getModel('ricardomartins_pagseguro/abstract');
-        $response = $model->getNotificationStatus($this->getRequest()->getPost('notificationCode'));
+        $response = $model->getNotificationStatus($notificationCode);
         if (false === $response) {
             Mage::throwException('Falha ao processar retorno XML do PagSeguro.');
         }
         $model->proccessNotificatonResult($response);
-
+        if (isset($response->reference)) {
+            $this->getResponse()->setBody('Notificação recebida para o pedido ' . (string)$response->reference);
+        }
 
     }
 }
